c4b3d76ee8182ad326ff65db9e74d779756c0259
Edgar Xolop <edgarxoix@gmail.com>
2017-02-17 21:51:32 -0600

    add method print, fixed error root and value null

--- src/xlp/util/genric/list/SinglyLinkedList.java
+++ src/xlp/util/genric/list/SinglyLinkedList.java
@@ -13,14 +13,24 @@ public class SinglyLinkedList <T> implements List<T> {
 	@Override
 	public void add(T t) {
 		if(root == null){
+			root = new Element<>();
 			root.setValue(t);
 			root.setPosition(UtilConstant.START_INDEX);
 		}else{
 			Element<T> newElement = new Element<>();
 			newElement.setNode(root);
+			newElement.setValue(t);
 			newElement.setPosition(root.getNextPosition());		
 				
 			root = newElement;
+		}
+	}
+	
+	public void printList(){
+		Element<T> record = root;
+		while (record != null) {
+			System.out.println(record.getValue());
+			record = (Element<T>) record.getNode();
 		}
 	}
 }
