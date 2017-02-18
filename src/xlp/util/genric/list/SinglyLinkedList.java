e74967b419fef3c6870df1b3c48919c37d7876bf
Edgar Xolop <edgarxoix@gmail.com>
2017-02-17 22:21:16 -0600

    configure method remove, set integer as position of item

--- src/xlp/util/genric/list/SinglyLinkedList.java
+++ src/xlp/util/genric/list/SinglyLinkedList.java
@@ -5,9 +5,11 @@ import xlp.util.genric.List;
 public class SinglyLinkedList <T> implements List<T> {
 	
 	private Element<T> root; //root Element
+	private Integer size;
 	
 	public SinglyLinkedList() {
 		root = null;
+		size = 0;
 	}
 	 
 	@Override
@@ -24,13 +26,46 @@ public class SinglyLinkedList <T> implements List<T> {
 				
 			root = newElement;
 		}
+		++size;
+	}
+	public void remove(Integer position){
+		Element<T> record = root;
+		Element<T> last = null;
+		while (record != null) {
+			if(record.getPosition().equals(position)){
+				if(last == null){
+					root = (Element<T>) record.getNode();
+
+					System.out.println("last null " + position);
+					record = null;
+				}else if(position.equals(UtilConstant.START_INDEX)){
+					System.out.println("position  0 " + position);
+					
+					last.setNode(null);
+					record = null;
+				}else{
+					System.out.println("position  " + position);
+					last.setNode(record.getNode());
+					record = null;
+				}
+				--size;
+			}else{
+				last = record;
+				record = (Element<T>) last.getNode();
+			}
+			
+		}
 	}
 	
 	public void printList(){
 		Element<T> record = root;
 		while (record != null) {
-			System.out.println(record.getValue());
+			System.out.println(record.getPosition() + "  " + record.getValue());
 			record = (Element<T>) record.getNode();
 		}
 	}
+	
+	public Integer getSize(){
+		return size;
+	};
 }
